5c1f8d08f4a8a4b3cca599590a51d5d4c326b44a
あららぎ <araragi.hoozuki@yandex.ru>
2021-08-09 15:42:00 +0800

    Add files via upload

---
+++ FehEditor/Encrypter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DSDecmp.Formats.Nitro;
+
+namespace FehEditor
+{
+    class Encrypter
+    {
+        private static byte[] LZ11Compress(byte[] decompressed)
+        {
+            using (MemoryStream dstream = new MemoryStream(decompressed))
+            {
+                using (MemoryStream cstream = new MemoryStream())
+                {
+                    (new LZ11()).Compress(dstream, decompressed.Length, cstream);
+                    return cstream.ToArray();
+                }
+            }
+        }
+
+        public static byte[] EncryptAndCompress(byte[] filedata)
+        {
+            var xorkey = BitConverter.ToUInt32(filedata, 0) >> 8;
+            xorkey *= 0x8083;
+            var lz = filedata.Skip(4).ToArray();
+            lz = LZ11Compress(lz);
+            var output = new byte[4 + lz.Length + (lz.Length % 4 == 0 ? 0 : 4 - lz.Length % 4)];
+            filedata.Take(4).ToArray().CopyTo(output, 0);
+            lz.CopyTo(output, 4);
+            for (var i = 8; i < output.Length; i += 0x4)
+            {
+                BitConverter.GetBytes(BitConverter.ToUInt32(output, i) ^ xorkey).CopyTo(output, i);
+                xorkey = BitConverter.ToUInt32(output, i);
+            }
+            return output;
+        }
+    }
+}
---
+++ FehEditor/JSON/SRPGMapJson.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FehEditor.JSON
+{
+    public class SRPGMapJson
+    {
+        public uint archive_size;
+        public uint ptr_list_offset;
+        public uint ptr_list_length;
+        public uint tag_list_length;
+        public uint uk1;
+        public uint uk2;
+        public ulong magic;
+        public uint uk3;
+        public uint highest_score;
+        public ulong addr_field;
+        public ulong addr_player_pos;
+        public ulong addr_units;
+        public uint player_count;
+        public uint unit_count;
+        public byte turns_to_win;
+        public byte last_enemy_phase;
+        public byte turns_to_defend;
+        public Field field;
+        public Position[] player_pos;
+        public Unit[] units;
+    }
+
+    public class Field
+    {
+        public string id;
+        public uint width;
+        public uint height;
+        public byte base_terrain;
+        public byte[][] terrain;
+        
+    }
+
+    public class Position
+    {
+        public ushort x;
+        public ushort y;
+    }
+
+    public class Unit
+    {
+        public string id_tag;
+        public string[] skills;
+        public string accessory;
+        public Position pos;
+        public byte rarity;
+        public byte lv;
+        public byte cooldown_count;
+        public byte uk = 100;
+        public Stats stats;
+        public byte start_turn;
+        public byte movement_group;
+        public byte movement_delay;
+        public byte break_terrain;
+        public byte tether;
+        public byte true_lv;
+        public byte is_enemy;
+        public string spawn_check;
+        public byte spawn_count;
+        public byte spawn_turns;
+        public byte spawn_target_remain;
+        public byte spawn_target_kills;
+    }
+
+    public class Stats
+    {
+        public short hp;
+        public short atk;
+        public short spd;
+        public short def;
+        public short res;
+    }
+
+}
